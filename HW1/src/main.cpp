7aa0e89cbb26a16e3b9f6d13c392d0be49724584
Nocturai <107352426+Nocturai@users.noreply.github.com>
2025-07-22 22:25:45 +0800

    Update main.cpp

--- HW1/src/main.cpp
+++ HW1/src/main.cpp
@@ -3,18 +3,20 @@
 using namespace std;
 
 int a(int m, int n) {
-    if (m == 0) return n + 1;
-    if (n == 0) return a(m - 1, 1);
-    return a(m - 1, a(m, n - 1));
+  if (m == 0)
+    return n + 1;
+  if (n == 0)
+    return a(m - 1, 1);
+  return a(m - 1, a(m, n - 1));
 }
 
 int main() {
-    int m,n;
-    
-    cout << "input m n" << endl;
-    cin >> m >> n;
-    cout << m << "," << n << "," << a(m,n) << endl;
+  int m, n;
 
-    return 0;
+  cout << "input m n" << endl;
+  cin >> m >> n;
+  cout << m << "," << n << "," << a(m, n) << endl;
+
+  system("pause");
+  return 0;
 }
-
